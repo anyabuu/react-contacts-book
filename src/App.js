755abd15c862242e268412ed5fcf8e0284bf18a2
Anya Bu <annabukhtiiarova@gmail.com>
2022-12-16 11:33:53 +0200

    works

--- src/App.js
+++ src/App.js
@@ -5,68 +5,25 @@ import Form from "./components/Form/Form";
 
 class App extends React.Component {
 
-
     state = {
         items: [],
 
-        name: '',
-        surname: '',
-        number: '',
-
-        formError: '',
-        formValid: true
+        // formError: '',
+        // formValid: true
     }
 
-    onInputNameChange = (e) => {
-        this.setState({ name: e.target.value });
-    }
-
-    onInputSurnameChange = (e) => {
-        this.setState({ surname: e.target.value });
-    }
-
-    onInputNumberChange = (e) => {
-        this.setState({ number: e.target.value });
-    }
-
-
-
-    onHandleSubmit = (e) => {
-        e.preventDefault();
-
-
-        console.log(this.state.name)
-
-        if (this.state.name.length === 0 || this.state.surname.length === 0 || this.state.number.length === 0) {
-            // this.setState({
-            //     formError: 'All fields are required',
-            //     formValid: false
-            // });
-            return;
-        }
+    handleAddItem = ({name, surname, number}) => {
 
         const newItem = {
-            name: this.state.name,
-            surname: this.state.surname,
-            number: this.state.number,
+            name: name,
+            surname: surname,
+            number: number,
             id: Date.now()
         };
-        this.setState({
-            items: [...this.state.items, newItem],
-            name: '',
-            surname: '',
-            number: '',
-            formError: '',
-            formValid: true
-        });
-    }
 
-    resetForm = () => {
-        this.setState({
-            name: '',
-            surname: '',
-            number: '',
-        })
+        this.setState(({items}) => ({
+            items: [...items, newItem],
+        }));
     }
 
     removeListItem = (e, id) => {
@@ -77,8 +34,6 @@ class App extends React.Component {
             items: result
         })
     }
-
-
 
     render() {
         return (
@@ -91,53 +46,8 @@ class App extends React.Component {
                     </div>
                 </header>
 
-
                 <ContactsList items={this.state.items} removeListItem={this.removeListItem}/>
-
-                <section className="form">
-                    <div className="container form__container">
-                        <h2 className="form__title">
-                            Create new contact
-                        </h2>
-                        <form className="form" onSubmit={this.onHandleSubmit}>
-                            <div className="form__input-wrapper">
-                                <input className="form__input"
-                                       value={this.state.name}
-                                       type="text"
-                                       name="name"
-                                       placeholder="Name"
-                                       onChange={this.onInputNameChange}
-                                />
-                                <input className="form__input"
-                                       value={this.state.surname}
-                                       type="text"
-                                       name="surname"
-                                       placeholder="Surname"
-                                       onChange={this.onInputSurnameChange}
-                                />
-                                <input className="form__input"
-                                       value={this.state.number}
-                                       type="number"
-                                       name="number"
-                                       placeholder="Phone number"
-                                       onChange={this.onInputNumberChange}
-                                />
-                            </div>
-                            <div className='form__error-message'>
-                                {this.state.formError}
-                            </div>
-                            <div className="form__button-wrapper">
-                                <button className="form__submit-button button" disabled={!this.state.formValid} >
-                                    OK
-                                </button>
-                                <button className="form__cancel-button button" type="reset" onClick={this.resetForm}>
-                                    Cancel
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </section>
-
+                <Form handleAddItem={this.handleAddItem}/>
 
             </>
         );
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,12 +2,103 @@ import React from "react";
 import './Form.css';
 
 class Form extends React.Component {
-    //
-    // render() {
-    //     return (
-    //
-    //     )
-    // }
+
+    state = {
+        name: '',
+        surname: '',
+        number: '',
+
+        formError: '',
+        formValid: true
+    }
+
+
+    onInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value ,
+            formValid: true
+        })
+    }
+
+    resetForm = () => {
+        this.setState({
+            name: '',
+            surname: '',
+            number: '',
+        })
+    }
+
+    onHandleSubmit = (e) => {
+        e.preventDefault();
+
+        if (this.state.name.length === 0 || this.state.surname.length === 0 || this.state.number.length === 0) {
+            this.setState({
+                formError: 'All fields are required',
+                formValid: false
+            });
+            return;
+        }
+
+        const {handleAddItem} = this.props
+
+        handleAddItem(this.state)
+
+        this.setState({
+            name: '',
+            surname: '',
+            number: '',
+            formError: '',
+            formValid: true
+        });
+    }
+
+    render() {
+        return (
+            <section className="form">
+                <div className="container form__container">
+                    <h2 className="form__title">
+                        Create new contact
+                    </h2>
+                    <form className="form" onSubmit={this.onHandleSubmit}>
+                        <div className="form__input-wrapper">
+                            <input className="form__input"
+                                   value={this.state.name}
+                                   type="text"
+                                   name="name"
+                                   placeholder="Name"
+                                   onChange={this.onInputChange}
+                            />
+                            <input className="form__input"
+                                   value={this.state.surname}
+                                   type="text"
+                                   name="surname"
+                                   placeholder="Surname"
+                                   onChange={this.onInputChange}
+                            />
+                            <input className="form__input"
+                                   value={this.state.number}
+                                   type="number"
+                                   name="number"
+                                   placeholder="Phone number"
+                                   onChange={this.onInputChange}
+                            />
+                        </div>
+                        <div className='form__error-message'>
+                            {this.state.formError}
+                        </div>
+                        <div className="form__button-wrapper">
+                            <button className="form__submit-button button" disabled={!this.state.formValid} >
+                                OK
+                            </button>
+                            <button className="form__cancel-button button" type="reset" onClick={this.resetForm}>
+                                Cancel
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Form;
